d1e2d8c9203f695f11f64fece20396f46df1c252
mandrix <ale.aazv@icloud.com>
2017-10-23 23:09:03 -0600

    crear el archivo

---
+++ progra_2.py
@@ -0,0 +1,21 @@
+		#cantidad de minas , si es multijugador o no
+def partida(nivel, multi):
+	coordena = input(el mouse)
+	if self.activo:
+		if multi:
+			
+
+
+class cuadro(inicio):
+	def __init__(self, x, y):
+		self.x = x
+		self.y = y
+		self.activo =  False
+		self.bandera = False 
+		self.mina = False 
+		self.minas_alrededor = 0
+	def click(self, jugador):
+		if self.activo:
+			if self.bandera:
+				if self.mina:
+					
