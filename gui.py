ddf62b2937358bcefdeb3f0aa77a34dc80c5ed86
Joseph Zamora <joseph.zamora64@yahoo.com>
2017-10-29 02:11:09 -0600

    Custom tkinter

--- gui.py
+++ gui.py
@@ -1,5 +1,35 @@
 from tkinter import *
 import progra_2
+import tkinter.messagebox
+
+def pedirCustom(key):
+   tkinter.messagebox.showinfo("Custom","Choose the settings you want for your custom match") 
+
+   containerCustom = Frame(root)
+   containerCustom.grid(row=1)
+
+   textA = int(StringVar())
+   labelAncho = Label(containerCustom, text="Width: ", fg=mainFg, bg=mainBg, font=mainFont, width = mainWidth)
+   entryAncho = Entry(containerCustom, textvariable=textA)
+   textL = int(StringVar())
+   labelLargo = Label(containerCustom, text="Height: ", fg=mainFg, bg=mainBg, font=mainFont, width = mainWidth)
+   entryLargo = Entry(containerCustom, textvariable=textL)
+   textM = int(StringVar())
+   labelMinas = Label(containerCustom, text="Mines: ", fg=mainFg, bg=mainBg, font=mainFont, width = mainWidth)
+   entryMinas = Entry(containerCustom, textvariable=textM)
+
+   readyButt = Button(containerCustom, text="Ready", fg=mainFg, bg=mainBg, font=mainFont, width=mainWidth, command=lambda: practica_2.main.ubicar_minas(0,ancho=textA.get(),largo=textL.get(),minas=textM.get()))
+
+   readyButt.grid(row=3,column=0)
+   labelAncho.grid()
+   entryAncho.grid(row=0, column=1)
+   labelLargo.grid(row=1, column=0)
+   entryLargo.grid(row=1, column=1)
+   labelMinas.grid(row=2, column=0)
+   entryMinas.grid(row=2, column=1)
+
+   
+
 def Game(players, multiplayer):
     menuFrame.destroy()
     container = Frame(root, bd=10, relief="groove")# freme kja :v
@@ -11,7 +41,9 @@ def Game(players, multiplayer):
     #30x16
     mode3Butt = Button(container, text="30x16", fg=mainFg,bg=mainBg,font=mainFont,width=mainWidth, command= lambda: progra_2.main.ubicar_minas(3))
     #Custom
-    mode4Butt = Button(container, text="Custom", fg=mainFg,bg=mainBg,font=mainFont,width=mainWidth, command= lambda: progra_2.main.ubicar_minas(0))
+    mode4Butt = Button(container, text="Custom", fg=mainFg,bg=mainBg,font=mainFont,width=mainWidth)
+
+    mode4Butt.bind("<Button-1>", pedirCustom)
 
 
     container.grid()
