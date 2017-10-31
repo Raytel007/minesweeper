e26fa12bcdcb17f17f6459be5672b6571387f278
mandrix <ale.aazv@icloud.com>
2017-10-31 02:26:05 -0600

    calculando alrededor

--- progra_2.py
+++ progra_2.py
@@ -5,68 +5,16 @@ from random import *
 10,11,12,13,14,
 15,16,17,18,19,
 20,21,22,23,24]
-""""
+"""
 class partida:
     def __init__(self):
         self.lista = []
         self.largo = 0
-
-    def alrededor_mina(self):
-                # [y, x]
-        arreglo = [[-1,-1],[-1,0],[-1,1],[0,-1],[0,1],[1,-1],[1,0],[1,1]]
-        for x in self.lista:
-            coordenada = self.lista.index(x)
-            alrededor = arreglo.copy()
-            if coordenada >= len(self.lista) - self.largo:
-                alrededor.pop()
-                alrededor.pop()
-                alrededor.pop()
-                print("uno")
-            if coordenada <= self.largo:
-                alrededor.pop(0)
-                alrededor.pop(0)
-                alrededor.pop(0)
-                print("dos")
-
-            if coordenada % self.largo == self.largo - 1:
-                try:
-                    alrededor.remove([0, 1])
-                except:
-                    print(1)
-
-                try:
-                    alrededor.remove([-1, 1])
-                except:
-                    print(2)
-
-                try:
-                    alrededor.remove([1, 1])
-                except:
-                    print(3)
-
-            if not coordenada % self.largo and coordenada != 0:
-                try:
-                    alrededor.remove([0, -1])
-                except:
-                    print(1)
-
-                try:
-                    alrededor.remove([-1, -1])
-                except:
-                    print(2)
-
-                try:
-                    alrededor.remove([1, -1])
-                except:
-                    print(3)
-            print("re",coordenada, alrededor)
     def ubicar_minas(self,dificultad, **customizado):  # creacion y ubicacion de la mina y lista, esto no va en esta class
         # prsado = 0 #   1    ,     2    ,     3
-        nivel = [[8, 8, 10], [16, 16, 40], [16, 30, 99]]
-
+        nivel = [[8,8 , 10], [16, 16, 40], [16, 30, 99]]
         if dificultad:
             ancho, largo, minas = nivel[dificultad - 1][0], nivel[dificultad - 1][1], nivel[dificultad - 1][2]
-
         else:
             ancho = customizado["ancho"]
             largo = customizado["largo"]
@@ -94,6 +42,47 @@ class cuadro(partida):
         self.mina = False 
         self.minas_alrededor = 0
         super().__init__()
+    def retornar_unos_y_ceros(self):
+        return self.minas_alrededor
+    def alrededor_mina(self):
+        arreglo = [[-1, -1], [-1, 0], [-1, 1], [0, -1], [0, 1], [1, -1], [1, 0], [1, 1]]
+
+        for x in main.lista:
+            coordenada = main.lista.index(x)
+            alrededor = arreglo.copy()
+            if coordenada >= len(main.lista) - main.largo:
+                alrededor.pop()
+                alrededor.pop()
+                alrededor.pop()
+            if coordenada < main.largo:
+                alrededor.pop(0)
+                alrededor.pop(0)
+                alrededor.pop(0)
+            if coordenada % main.largo == main.largo - 1:
+                alrededor.remove([0, 1])
+                try:
+                    alrededor.remove([-1, 1])
+                except:
+                    pass
+                try:
+                    alrededor.remove([1, 1])
+                except:
+                    pass
+            if not coordenada % main.largo:
+                alrededor.remove([0, -1])
+                try:
+                    alrededor.remove([-1, -1])
+                except:
+                    pass
+                try:
+                    alrededor.remove([1, -1])
+                except:
+                    pass
+            for y in alrededor:
+                if main.lista[main.lista.index(x) + y[0] * main.largo + y[1]].mina:
+                    main.lista[main.lista.index(x)].minas_alrededor += 1
+
+
 
     def click(self, click):
         #derecho activa casilla
@@ -110,9 +99,8 @@ class cuadro(partida):
         else:
             self.bandera = not self.bandera
 
-            #vecino = self.lista[self.lista(index(x) ]
+        #vecino = self.lista[self.lista(index(x) ]
 
 main = partida()
 main.ubicar_minas(1)
-print(main.retornando_lista())
-main.alrededor_mina()
+main.lista[0].alrededor_mina()
